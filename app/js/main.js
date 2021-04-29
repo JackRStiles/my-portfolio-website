3fc55eea734ffde26778540a822784859014143b
Jack <jackrstiles@outlook.com>
2021-04-29 22:53:49 +0100

    tweaked hero size and added burger script

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,11 @@
-var dingDong = "true";
-console.log(dingDong);
+document.getElementById('navbar-toggle').addEventListener('click', toggleNav);
+
+function toggleNav() {
+    var nav = document.getElementById('navbarMenuHeroA');
+    var className = nav.getAttribute('class');
+    if (className == 'navbar-menu') {
+        nav.className = 'navbar-menu is-active';
+    } else {
+        nav.className = 'navbar-menu';
+    }
+}
